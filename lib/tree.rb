ed36639d4eb38792adfb9f86f6c8a093fb5e18c9
Schwad <nicholas.schwaderer@gmail.com>
2015-02-07 23:44:57 +0000

    make modular

--- lib/tree.rb
+++ lib/tree.rb
@@ -7,16 +7,34 @@ class Tree
     @doc_string = File.open(filename, "r") {|f| f.read}
     cleanup_string
 
-    name = /^<(\w+)\s*.*>/.match(@doc_string)[1]
-    text = /<#{name}.*?>(.*?)</.match(@doc_string)[1]
-    nested_text = /<#{name}.*?>(.*?)<\/#{name}>/.match(@doc_string)[1]
-    class_info = /<#{name} class="(.*?)">/.match(@doc_string)
-    tag_class = class_info[1].split(" ") if class_info
-    id_info = /<#{name} id="(.*?)">/.match(@doc_string)
-    tag_id = id_info[1].split(" ") if id_info
-    # binding.pry
+    @document = Tag.new("document", nil, nil, nil, @doc_string)
+    queue = [@document]
+    until queue.empty?
+      current_node = queue.shift
+      if has_children?(current_node)  
+        #break the nested text into children
+        #create child node for each tag 
+        #add child node to current_node's child array
+      end
 
-    @root = Tag.new(name,text,tag_class,tag_id,nested_text)
+      #add new children tags to queue
+    end
+  end
+
+  def has_children?(node)
+    node.nested_text =~ /</
+  end
+
+  def create_child(parent_node, string)
+    name = /^<(\w+)\s*.*>/.match(string)[1]
+    text = /<#{name}.*?>(.*?)</.match(string)[1]
+    nested_text = /<#{name}.*?>(.*?)<\/#{name}>/.match(string)[1]
+    class_info = /<#{name} class="(.*?)">/.match(string)
+    tag_class = class_info[1].split(" ") if class_info
+    id_info = /<#{name} id="(.*?)">/.match(string)
+    tag_id = id_info[1].split(" ") if id_info
+
+    Tag.new(name,text,tag_class,tag_id,nested_text,nil,parent_node)
   end
 
   def cleanup_string
