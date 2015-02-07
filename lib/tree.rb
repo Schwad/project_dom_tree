94906fef9185797c4f5b8c75908127f49daa3410
Dan Berger <dsberger@gmail.com>
2015-02-07 13:42:40 -0800

    minor edits to Tree initialize

--- lib/tree.rb
+++ lib/tree.rb
@@ -5,10 +5,13 @@ class Tree
   attr_accessor :doc_string, :root
   def initialize(filename)
     @doc_string = File.open(filename, "r") {|f| f.read}
+
+    # extract these scrubbing lines into one method
     @doc_string.gsub!("<!doctype html>\n", "")
     @doc_string.gsub!(/\n\s*/, "")
+
     name = /^<(\w+)\s*.*>/.match(@doc_string)[1]
-    text = /<#{name}>(.*)<\/#{name}>/.match(@doc_string)[1]
+    text = /<#{name}.*>(.*)<\/#{name}>/.match(@doc_string)[1]
     @root = Tag.new(name,text)
   end
 end
